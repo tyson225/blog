90a89a6744ecb28da28b4c4538244582adf6c5b4
Tyson McDonald <tyson22509@hotmail.com>
2022-03-28 22:27:19 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// app
+const app = express();
+
+// database
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+}).then(() => console.log('connected to mongodb'));
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+app.use(cors());
+
+// routes
+app.get('/api', (req, res) => {
+  res.json({ time: Date().toString() });
+});
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
